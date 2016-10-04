bfd638d4f02ad2d8f6a37aaa2364d0a6f8945578
nmn-notes <nmn.notes@gmail.com>
2016-10-03 20:00:43 -0700

    # Refactor code.

--- algorithms/src/main/java/org/nmn/notes/util/StringUtils.java
+++ algorithms/src/main/java/org/nmn/notes/util/StringUtils.java
@@ -3,12 +3,19 @@ package org.nmn.notes.util;
 import static org.nmn.notes.util.Constants.EMPTY_STRING;
 
 /**
- * Utility class for String
- * 
+ * Utility class for String.
+ *
  * @author nmn-notes
  */
 public final class StringUtils {
 
+	/**
+	 * Checks if given {@link String} is blank or empty.
+	 *
+	 * @param str the str
+	 * @return <code>true</code>, if is blank or empty;
+	 * 		   <code>false</code> otherwise.
+	 */
 	public static boolean isBlankOrEmpty(final String str) {
 		return (null == str || EMPTY_STRING.equals(str.trim())) ? Boolean.TRUE : Boolean.FALSE;
 	}
