4cc126c900099e140d5cb6a9765855bd3cff5882
nmn-notes <nmn.notes@gmail.com>
2016-10-10 20:22:03 -0700

    # Check is String s2 is a substring of String s1.

--- algorithms/src/main/java/org/nmn/notes/algorithms/string/Search.java
+++ algorithms/src/main/java/org/nmn/notes/algorithms/string/Search.java
@@ -1,5 +1,8 @@
 package org.nmn.notes.algorithms.string;
 
+import static java.lang.Boolean.TRUE;
+import static java.lang.Boolean.FALSE;
+
 /**
  * String search algorithms.
  * 
@@ -8,17 +11,42 @@ package org.nmn.notes.algorithms.string;
 public class Search {
 
 	/**
-	 * Checks if the given str1 is prefix of str2
+	 * Checks if the given str2 is prefix of str1.
 	 * 
-	 * @param str1 prefix {@link String}
-	 * @param str2 {@link String}
-	 * @return <code>true</code> if str1 is prefix of str2;
-	 * 		   <code>false</code> otherwise
+	 * @param str1 main {@link String}
+	 * @param str2 prefix {@link String}
+	 * @return <code>true</code> if str2 is prefix of str1;
+	 * 		   <code>false</code> otherwise.
 	 */
 	public static boolean hasPrefix(final String str1, final String str2){
-		if (str1 == null || str2 == null || str1.length() > str2.length()) {
-			return Boolean.FALSE;
+		if (str1 == null || str2 == null || str1.length() < str2.length()) {
+			return FALSE;
 		}
-		return str1.hashCode() == str2.substring(0, str1.length()).hashCode() ? Boolean.TRUE : Boolean.FALSE;
+		return str1.substring(0, str2.length()).hashCode() == str2.hashCode() ? TRUE : FALSE;
+	}
+
+	/**
+	 * Check if {@link String} s2 is a substring of {@link String} s1.
+	 * 
+	 * @param s1 main {@link String}.
+	 * @param s2 substring {@link String}.
+	 * @return <code>true</code> if s2 is a substring of s1;
+	 * 		   <code>false</code> otherwise.
+	 * @throws Exception if any.
+	 */
+	public static boolean contains(String s1, String s2) {
+		if (s1 == null || s2 == null || s1.length() < s2.length()) {
+			return FALSE;
+		}
+
+		final int s2HashCode = s2.hashCode();
+
+		for (int i = 0; i < s1.length() - s2.length(); i++) {
+			System.out.println("Evaulating string: " + s1.substring(i, i + s2.length()));
+			if (s1.substring(i, i + s2.length()).hashCode() == s2HashCode) {
+				return TRUE;
+			}
+		}
+		return FALSE;
 	}
 }
--- algorithms/src/test/java/org/nmn/notes/algorithms/string/SearchTest.java
+++ algorithms/src/test/java/org/nmn/notes/algorithms/string/SearchTest.java
@@ -6,22 +6,30 @@ import static org.testng.Assert.assertFalse;
 import org.testng.annotations.Test;
 
 /**
- * Test class for {@link Search}
+ * Test class for {@link Search}.
+ * 
  * @author nmn-notes
  */
 public class SearchTest {
 
 	/**
-	 * Test hasPrefix
+	 * Test is second {@link String} is a prefix of first {@link String}.
 	 */
 	@Test
 	public void testHasPrefix() {
-		assertTrue(Search.hasPrefix("abc", "abcdef"));
-		assertFalse(Search.hasPrefix("abc", "defabc"));
+		assertTrue(Search.hasPrefix("abcdef", "abc"));
+		assertFalse(Search.hasPrefix("defabc", "abc"));
 		assertTrue(Search.hasPrefix("abc", "abc"));
-		assertFalse(Search.hasPrefix(null, "abcdef"));
-		assertFalse(Search.hasPrefix("abc", null));
-		assertFalse(Search.hasPrefix("abcdef", "abc"));
-		
+		assertFalse(Search.hasPrefix("abcdef", null));
+		assertFalse(Search.hasPrefix(null, "abc"));
+		assertFalse(Search.hasPrefix("abc", "abcdef"));
+	}
+
+	/**
+	 * Test if second {@link String} contains in first {@link String}.
+	 */
+	@Test
+	public void testContains() {
+		assertTrue(Search.contains("abcdef", "ef"));
 	}
 }
