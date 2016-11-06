512698a0139708d67c3e5db1ae92ca040d78ffe6
nmn-notes <nmn.notes@gmail.com>
2016-11-05 23:43:07 -0700

    DP approach to solve coin change problem to determing maximum number of ways change can be made for a given value and coins.

--- algorithms/src/main/java/org/nmn/notes/algorithms/dp/CoinChange.java
+++ algorithms/src/main/java/org/nmn/notes/algorithms/dp/CoinChange.java
@@ -20,12 +20,12 @@ public class CoinChange {
 	 * b. Solution does contains mth coin Sm.
 	 * The time complexity of this algorithm is exponential O(2^n).
 	 * 
-	 * @param S array of valued coins.
+	 * @param C array of valued coins.
 	 * @param m number of coins available for the change.
 	 * @param n value for which change is calculated.
 	 * @return the count of number of ways change can be made.
 	 */
-	public static int count(int [] S, int m, int n) {
+	public static int count(int [] C, int m, int n) {
 		//Value is 0, one solution found.
 		if (n == 0) {
 			return 1;
@@ -43,9 +43,49 @@ public class CoinChange {
 
 		/**
 		 * Count is sum of:
-		 * 1. current coin is NOT included, which does NOT change the value.
-		 * 2. current coin is included, which reduces the value by current coin value.
+		 * 1. Current coin is NOT included, which does NOT change the value.
+		 * 2. Current coin is included, which reduces the value by current coin value.
 		 */
-		return count(S, m - 1, n) + count(S, m, n - S[m-1]);
+		return count(C, m - 1, n) + count(C, m, n - C[m-1]);
+	}
+
+	/**
+	 * Use DP approach to get the number of ways we can make change for value n.
+	 * 
+	 * This DP approach will use lookup table to first calculate and store number of ways change can be with with 1st coin,
+	 * and update the number of ways change is made with introduction of a new coin.
+	 * 
+	 * The time complexity of this algorithm is O(n * number of coins) and space complexity of O(n).
+	 * 
+	 * @param C array of valued coins.
+	 * @param n value for which change is calculated.
+	 * @return the count of number of ways change can be made.
+	 */
+	public static int count(int [] C, int n) {
+		//Lookup table, n+1 to store base case of value 0.
+		int count [] = new int[n + 1];
+
+		//Base case, when value is 0, 1 solution exists.
+		count[0] = 1;
+
+		//Introduce a new coin and re-calculate all values which are higher than current coin value.
+		for (int i = 0; i < C.length; i++) {
+			//Add number of ways change can be made with NEW coin. Only values greater than current coin will have another way to get change.
+			for (int j = C[i]; j <= n; j++) {
+				// New total number of ways change can be made for this value is sum of:
+				count[j] = 
+					// Existing number of ways change was made WITHOUT this coin.
+					count[j] + 
+					// Total number of ways change can be made WITH this coin which is equal to:
+					// Total number of ways change can be made for a value less than coin value, since for every
+					// such way if we add current coin, we will get current value.
+					// For example if current considered coin is 8 from a coin set of {1, 2, 8} and current value is 10
+					// Number of ways change be made for 2 is {1, 1} and {2}
+					// Number of ways change can be made for 10 is by adding 10 to every way change was made for 2: {1, 1, 8}, {2, 8}
+					count[j - C[i]];
+			}
+		}
+
+		return count[n];
 	}
 }
--- algorithms/src/test/java/org/nmn/notes/algorithms/dp/CoinChangeTest.java
+++ algorithms/src/test/java/org/nmn/notes/algorithms/dp/CoinChangeTest.java
@@ -16,25 +16,53 @@ public class CoinChangeTest {
 	 */
 	@Test
 	public void testCount() {
-		int [] S = {1, 2, 3};
+		int [] C = {1, 2, 3};
 		//{1, 1, 1}, {1, 2}, {3}
-		assertEquals(CoinChange.count(S, S.length, 3), 3);
+		assertEquals(CoinChange.count(C, C.length, 3), 3);
 
 		//{1, 1, 1, 1}, {1, 1, 2}, {2, 2}, {1, 3}
-		assertEquals(CoinChange.count(S, S.length, 4), 4);
+		assertEquals(CoinChange.count(C, C.length, 4), 4);
 
-		S = new int [] {2, 3, 5, 7, 8};
+		C = new int [] {2, 3, 5, 7, 8};
 
 		//{2}
-		assertEquals(CoinChange.count(S, S.length, 2), 1);
+		assertEquals(CoinChange.count(C, C.length, 2), 1);
 
 		//no solution since the lowest value coin is 2.
-		assertEquals(CoinChange.count(S, S.length, 1), 0);
+		assertEquals(CoinChange.count(C, C.length, 1), 0);
 
 		//{2, 3}, {5}
-		assertEquals(CoinChange.count(S, S.length, 5), 2);
+		assertEquals(CoinChange.count(C, C.length, 5), 2);
 
 		//{2,2,2,2,2}, {2,2,3,3}, {2,3,5}, {5,5}, {3,7}, {2,8}
-		assertEquals(CoinChange.count(S, S.length, 10), 6);
+		assertEquals(CoinChange.count(C, C.length, 10), 6);
 	}
+
+	/**
+	 * Test coin count DP approach.
+	 */
+	@Test
+	public void testCountDP() {
+		int [] C = {1, 2, 3};
+		//{1, 1, 1}, {1, 2}, {3}
+		assertEquals(CoinChange.count(C, 3), 3);
+
+		//{1, 1, 1, 1}, {1, 1, 2}, {2, 2}, {1, 3}
+		assertEquals(CoinChange.count(C, 4), 4);
+
+		C = new int [] {2, 3, 5, 7, 8};
+
+		//{2}
+		assertEquals(CoinChange.count(C, 2), 1);
+
+		//no solution since the lowest value coin is 2.
+		assertEquals(CoinChange.count(C, 1), 0);
+
+		//{2, 3}, {5}
+		assertEquals(CoinChange.count(C, 5), 2);
+
+		//{2,2,2,2,2}, {2,2,3,3}, {2,3,5}, {5,5}, {3,7}, {2,8}
+		assertEquals(CoinChange.count(C, 10), 6);
+	}
+
 }
