635598f5d0e0f22be2019bd183e37f216b571420
nmn-notes <nmn.notes@gmail.com>
2016-11-26 22:48:00 -0800

    Find sum of middle elements after merging two sorted array.

--- algorithms/src/main/java/org/nmn/notes/algorithms/arrays/Merge.java
+++ algorithms/src/main/java/org/nmn/notes/algorithms/arrays/Merge.java
@@ -52,4 +52,52 @@ public class Merge {
 		return arr[mid - 1] + arr[mid];
 	}
 
+	/**
+	 * Given two sorted integer arrays, return sum of middle elements
+	 * of array obtained after merging the given array.
+	 * 
+	 * Complexity of algorithm is O(n + m)
+	 * 
+	 * @param arr1 first integer array.
+	 * @param arr2 second integer array.
+	 * @return sum of middle elements after merging arr1 and arr2.
+	 */
+	public static int sumOfMiddleElements(int [] arr1, int [] arr2) {
+		int indexOfMiddleElement = (arr1.length + arr2.length) / 2;
+		int i = 0;
+		int j = 0;
+		int k = 0;
+
+		int firstElement = 0;
+		int secondElement = 0;
+
+		while (k <= indexOfMiddleElement && i < arr1.length && j < arr2.length) {
+			firstElement = secondElement;
+			if (arr1[i] < arr2[j]) {
+				secondElement = arr1[i];
+				i++;
+			}
+			else {
+				secondElement = arr2[j];
+				j++;
+			}
+			k++;
+		}
+
+		while(k <= indexOfMiddleElement && i < arr1.length) {
+			firstElement = secondElement;
+			secondElement = arr1[i];
+			i++;
+			k++;
+		}
+
+		while(k <= indexOfMiddleElement && j < arr2.length) {
+			firstElement = secondElement;
+			secondElement = arr2[j];
+			j++;
+			k++;
+		}
+
+		return firstElement + secondElement;
+	}
 }
--- algorithms/src/test/java/org/nmn/notes/algorithms/arrays/MergeTest.java
+++ algorithms/src/test/java/org/nmn/notes/algorithms/arrays/MergeTest.java
@@ -15,7 +15,7 @@ public class MergeTest {
 	 * Test sum of middle elements of two sorted integer array.
 	 */
 	@Test
-	public void testSumOfMiddleElements() {
+	public void testSumOfMiddleElementsBF() {
 		int [] arr1 = {1, 3};
 		int [] arr2 = {2, 4};
 		assertEquals(Merge.sumOfMiddleElementsBF(arr1, arr2), 5);
@@ -28,5 +28,24 @@ public class MergeTest {
 		arr1 = new int[] {1};
 		arr2 = new int[] {1};
 		assertEquals(Merge.sumOfMiddleElementsBF(arr1, arr2), 2);
+	}
+
+	/**
+	 * Test sum of middle elements of two sorted integer array.
+	 */
+	@Test
+	public void testSumOfMiddleElements() {
+		int [] arr1 = {1, 3};
+		int [] arr2 = {2, 4};
+		assertEquals(Merge.sumOfMiddleElements(arr1, arr2), 5);
+
+		arr1 = new int[] {2, 3, 4, 5};
+		arr2 = new int[] {6, 7, 8, 9};
+		assertEquals(Merge.sumOfMiddleElements(arr1, arr2), 11);
+		assertEquals(Merge.sumOfMiddleElements(arr2, arr1), 11);
+
+		arr1 = new int[] {1};
+		arr2 = new int[] {1};
+		assertEquals(Merge.sumOfMiddleElements(arr1, arr2), 2);
 	}
 }
